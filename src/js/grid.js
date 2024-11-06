994dfdd07b3e08df754d8b99209093316adcc8a3
FinnNite <seawolf133700@gmail.com>
2024-11-06 23:30:54 +0400

    Косметические изменения

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,22 +1,23 @@
 // src/js/grid.js
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Сетка
-export const Grid = ({ cellSize }) => {
-    // Линии сетки
+export const Grid = ({ cellSize, windowSize }) => {
+  // Изменение размеров окна
+  // Линии сетки
   const gridLines = [];
 
-  for (let i = 0; i <= window.outerWidth; i += cellSize) {
+  for (let i = 0; i <= windowSize.width; i += cellSize) {
     gridLines.push(
-      <line key={`v-${i}`} className="grid-line" x1={i} y1={0} x2={i} y2={window.outerHeight} />
+      <line key={`v-${i}`} className="grid-line" x1={i} y1={0} x2={i} y2={windowSize.height} />
     );
     gridLines.push(
-      <line key={`h-${i}`} className="grid-line" x1={0} y1={i} x2={window.outerWidth} y2={i} />
+      <line key={`h-${i}`} className="grid-line" x1={0} y1={i} x2={windowSize.width} y2={i} />
     );
   }
 
   return (
-    <svg className="grid" width={window.outerWidth * 0.88} height={window.outerHeight*0.7}>
+    <svg id="grid-diagram-ignore" className="grid-diagram" width={windowSize.width} height={windowSize.height*0.8}>
       <g stroke="black" strokeWidth={1} opacity="10%">{gridLines}</g>
     </svg>
   );
