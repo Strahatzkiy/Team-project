855e92835c4a372cdf5ffdc5d28089f779faeb2f
FinnNite <seawolf133700@gmail.com>
2024-11-06 23:31:27 +0400

    Убрана сетка на фоне диаграммы

--- src/js/download.js
+++ src/js/download.js
@@ -4,11 +4,15 @@ import { saveAs } from "file-saver";
 
 export const handleDownload = () => {
     const workspace = document.getElementById("workspace");
+    const ignoreElements = document.getElementById("grid-diagram-ignore");
+    const originalOpacity = ignoreElements.style.opacity;
+    ignoreElements.style.opacity = '0';
     html2canvas(workspace).then((canvas) => {
       canvas.toBlob((blob) => {
         saveAs(blob, "diagram.png");
       });
     });
+    ignoreElements.style.opacity = originalOpacity;
   };
 
   export default handleDownload;
