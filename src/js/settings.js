71074235ad816e9b616dfecdccdcf34fe9dfd3b3
FinnNite <seawolf133700@gmail.com>
2024-11-07 22:06:19 +0400

    Добавление настроек

---
+++ src/js/settings.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import '../css/settings.css'; 
+export const Settings = ({ isOpen, onClose }) => {
+  useEffect(() => {
+    if (!isOpen) return;
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+  const handleClearCache = () => {
+    localStorage.removeItem("shapes-idef0");
+  };
+
+  if (!isOpen) return null;
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Настройки</h2>
+        <button onClick={handleClearCache}>Удалить кэш</button>
+        <button onClick={onClose}>Закрыть</button>
+      </div>
+    </div>
+  );
+};
+
+
+export default Settings;
