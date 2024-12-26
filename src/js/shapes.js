ae100a4d041a34153483179607de07f5c140382c
FinnNite <seawolf133700@gmail.com>
2024-12-27 00:18:53 +0400

    Изменения, связанные со стрелками

--- src/js/shapes.js
+++ src/js/shapes.js
@@ -1,11 +1,29 @@
 import "../css/shapes.css"
-import { useEffect, useState, useRef } from "react";
+import { createContext, useContext, useEffect, useState, useRef } from "react";
 import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import AutowidthInput from "react-autowidth-input";
 import invariant from "tiny-invariant";
 
+const RectangleContext = createContext();
+
+export const RectangleProvider = ({ children }) => {
+  const [rectangle, setRectangle] = useState(null);
+
+  const handleHover = (rect) => {
+    setRectangle(rect);
+  };
+
+  return (
+    <RectangleContext.Provider value={{ rectangle, handleHover }}>
+      {children}
+    </RectangleContext.Provider>
+  );
+};
+
+
 // Прямоугольник
-export const Rectangle = ({ id, width, height, content, canDrag, canType, onTextChanged, onDoubleClick, onBlur, isBig }) => {
+export const Rectangle = ({ id, x, y, width, height, content, canDrag, canType, onTextChanged, onDoubleClick, onBlur, isBig }) => {
+    const { handleHover } = useContext(RectangleContext);
     const ref = useRef(null);
     const [isDragging, setIsDragging] = useState(false);
     const [text, setText] = useState(content);
@@ -62,6 +80,8 @@ export const Rectangle = ({ id, width, height, content, canDrag, canType, onText
                 position: 'relative', // Для абсолютного позиционирования
             }}
             onDoubleClick={handleDoubleClick} 
+            onMouseEnter={() => handleHover({ x, y, width, height })}
+            onMouseLeave={() => handleHover(null)} 
         >
             {
                 (isBig ? (isEditing && canType ?
@@ -116,19 +136,32 @@ export const Rectangle = ({ id, width, height, content, canDrag, canType, onText
     );
   }
 
+// =====================================================================================================================================
 
-
-  export const RectangleWithArrows = ({id, width, height, content, canDrag, canType, onTextChanged, addArrow, addRectangle, isBig}) => {
+  export const RectangleWithArrows = ({id, x, y, width, height, content, canDrag, canType, onTextChanged, addArrow, addRectangle, isBig, childRectangle }) => {
     const ref = useRef(null);
     const [text, setText] = useState(content);
-    const [showArrows, setShowArrows] = useState(true); // Состояние для управления видимостью кнопок
+    const [showButtons, setShowButtons] = useState(true); // Состояние для управления видимостью кнопок
+    const [isDragging, setIsDragging] = useState(false);
+
+    useEffect(() => {
+      const rectE1 = ref.current;
+      invariant(rectE1);
+      if (canDrag) {
+          return draggable({
+              element: rectE1,
+              onDragStart: () => setIsDragging(true),
+              onDrop: () => setIsDragging(false),
+          });
+      }
+  }, [canDrag]);
 
     const addRectangleToWorkspace = () => { 
       console.log(id);
         let rectangleId = parseInt(id.match(/\d+/)[0]) + 1;
         console.log(rectangleId);
         addRectangle(rectangleId);
-  };
+    };
 
     const addArrowToWorkspace = (direction) => {
       const centerX = width / 2;
@@ -156,18 +189,29 @@ export const Rectangle = ({ id, width, height, content, canDrag, canType, onText
   };
 
   const handleDoubleClick = () => {
-    setShowArrows(false); // Скрыть кнопки при двойном щелчке
+    setShowButtons(false); // Скрыть кнопки при двойном щелчке
   };
   
   const handleBlur = () => {
-    setShowArrows(true); // Показать кнопки при уводе фокуса
+    setShowButtons(true); // Показать кнопки при уводе фокуса
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleBlur();
+      onTextChanged(text);
+    }
+    if (e.key === "Escape") {
+      handleBlur();
+    }
+  }
+
     return (
-      <div>
+      <div ref={ref}>
         <Rectangle
-          ref={ref}
           id={id}
+          x={x}
+          y={y}
           width={width}
           height={height}
           content={content}
@@ -177,59 +221,31 @@ export const Rectangle = ({ id, width, height, content, canDrag, canType, onText
           isBig={isBig}
           onDoubleClick={handleDoubleClick}
           onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
         />
-        <div>
-        {showArrows && isBig ? (
+        {showButtons && isBig ? (
                 <div>
                   <button className="top-button diagram-ignore add-arrow" onClick={() => addArrowToWorkspace('up')}></button>
                   <button className="bottom-button diagram-ignore add-arrow" onClick={() => addArrowToWorkspace('down')}></button>
                   <button className="left-button diagram-ignore add-arrow" onClick={() => addArrowToWorkspace('left')}></button>
                   <button className="right-button diagram-ignore add-arrow" onClick={() => addArrowToWorkspace('right')}></button>
-                </div> ) : (<div>
+                </div> ) : (!childRectangle && !isBig ? (<div>
                   <button className="right-button-small diagram-ignore add-arrow" onClick={() => addRectangleToWorkspace('right')}></button>
-            </div>)
+            </div>) : <div />)
             }
-
         </div>
-      </div>
     );
-}
-  
-// Овал
-export const Oval = ({width, height, canDrag}) => {
-  // Сохраняем объект в ref
-    const ref = useRef(null);
-    // Состояние переноса овала
-    const [isDragging, setIsDragging] = useState(false);
-  
-    // Эффект при переносе овала
-    useEffect(() => {
-      const ovalE1 = ref.current;
-      invariant(ovalE1);
-      if (canDrag) {
-        return draggable({
-          element: ovalE1,
-          onDragStart: () => setIsDragging(true),
-          onDrop: () => setIsDragging(false),
-        });
-      };
-    });
-  
-    return (
-      <div
-        ref={ref}
-        id={"shape"}
-        className={`oval ${isDragging ? "dragging" : ""}`}
-      />
-    )
 }
 
 // Стрелка
-export const Arrow = ({id, x1, y1, x2, y2, canDrag, initialText, side, canType, onTextChanged, deleteArrow}) => {
+export const Arrow = ({id, x1, y1, x2, y2, canDrag, initialText, side, canType, onTextChanged, addArrow, deleteArrow}) => {
   const ref = useRef(null);
+  const { rectangle } = useContext(RectangleContext);
   const [isDragging, setIsDragging] = useState(false);
   const [text, setText] = useState(initialText);
   const [isEditing, setIsEditing] = useState(false);
+  const [startCoords, setStartCoords] = useState(null);
+  const [currentCoords, setCurrentCoords] = useState(null); 
 
   useEffect(() => {
     const arrowE1 = ref.current;
@@ -241,7 +257,73 @@ export const Arrow = ({id, x1, y1, x2, y2, canDrag, initialText, side, canType, 
         onDrop: () => setIsDragging(false),
       });
     }
-  });
+  }); 
+
+  const getRectangleParams = () => {
+    console.log(rectangle);
+    if (rectangle) {
+      return [rectangle.x, rectangle.y, rectangle.width, rectangle.height];
+    }
+    return null;
+  };
+
+  const handleArrowClick = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const startX = e.clientX - rect.left;
+    const startY = e.clientY - rect.top;
+    console.log(startX);
+    console.log(startY);
+    if (!startCoords) {
+      setStartCoords({ x: startX, y: startY });
+    }
+    setCurrentCoords(null); 
+    console.log(startCoords);
+    console.log(currentCoords);
+
+    const handleMouseMove = (event) => {
+      setCurrentCoords({ x: event.clientX - rect.left, y: event.clientY - rect.top });
+      console.log(startCoords);
+      console.log(currentCoords);
+    };
+
+    const handleMouseUp = () => {
+      console.log(startCoords);
+      console.log(currentCoords);
+      const rectangleHovered = getRectangleParams();
+      console.log(rectangle);
+      if (currentCoords.x > rectangleHovered.x && currentCoords.x < rectangleHovered.x + rectangleHovered.width 
+        && currentCoords.y > rectangleHovered.y && currentCoords.y < rectangleHovered.y + rectangleHovered.height) {
+        addArrow(startCoords.x, startCoords.y, currentCoords.x, currentCoords.y);
+      }
+      setStartCoords(null);
+      setCurrentCoords(null);
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+    
+    if (startCoords) {
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    }
+  };
+  
+  useEffect(() => {
+    const handleMouseMoveEvent = (event) => {
+      setCurrentCoords({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMoveEvent);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMoveEvent);
+    };
+  }, []);
+
+  const handleRectangleClick = (rectangleX, rectangleY) => {
+    if (startCoords) {
+      addArrow(startCoords.x, startCoords.y, rectangleX, rectangleY); // Добавляем новую стрелку
+      setStartCoords(null); // Сбросить начальные координаты
+    }
+  };
 
   const updateDiagram = () => {
     window.dispatchEvent(new Event('resize'));
@@ -269,9 +351,13 @@ export const Arrow = ({id, x1, y1, x2, y2, canDrag, initialText, side, canType, 
     if (e.key === "Enter") {
       onTextChanged(text); // Сохраняем текст при нажатии Enter
       setIsEditing(false);
+      setStartCoords(null);
+      setCurrentCoords(null);
     }
     if (e.key === "Escape") {
       setIsEditing(false);
+      setStartCoords(null);
+      setCurrentCoords(null);
     }
   }
   
@@ -282,23 +368,25 @@ export const Arrow = ({id, x1, y1, x2, y2, canDrag, initialText, side, canType, 
     setIsEditing(false);
   }
 
+  const controlPoint1 = { x: (x1 + x2) / 2, y: y1 };
+  const controlPoint2 = { x: (x1 + x2) / 2, y: y2 };
 
   return (
-    <div className="arrow-container" onDoubleClick={handleDoubleClick}>
+    <div className="arrow-container" onDoubleClick={handleDoubleClick} onClick={handleArrowClick}>
       <svg width={Math.abs(x2-x1)+20} height={Math.abs(y2-y1)+20} style={{position: "absolute", x: 20, y: 20}}>
         <defs>
           <marker id="arrow" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto">
             <polygon points="0 0, 10 3.5, 0 7" fill="black" />
           </marker>
         </defs>
-        <line ref={ref}
+        <path ref={ref}
           id={id}
-          x1={x1}
-          y1={y1}
-          x2={x2}
-          y2={y2}
+          d={`M ${x1} ${y1} C ${controlPoint1.x} ${controlPoint1.y}, ${controlPoint2.x} ${controlPoint2.y}, ${x2} ${y2}`}
           markerEnd="url(#arrow)"
           className={`arrow${isDragging ? " dragging" : ""}`}
+          fill="none"
+          stroke="black"
+          strokeWidth="2"
         />
       </svg>
       {
